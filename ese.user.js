2cd0ebeacf406e5838272f95346127508be8d514
nalabjp <nalabjp@gmail.com>
2017-07-18 13:52:44 +0900

    Bump verstion to v1.1.0

--- ese.user.js
+++ ese.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Esa Search Extension
 // @namespace    ese
-// @version      1.0.1
+// @version      1.1.0
 // @description  Esa Search Extension makes advanced searching easy.
 // @author       nalabjp
 // @match        https://*.esa.io/*
